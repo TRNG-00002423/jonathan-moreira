0bace17302013944c6c9cb7681364163509c9126
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-02 13:40:07 -0400

    Variable Exploration

---
+++ Week1-Git-Python/exercises/Day1/qa-metrics-calculator/starter_code/type_explorer.py
@@ -0,0 +1,12 @@
+age = 28
+price = 19.99
+name = "Alice"
+is_active = True
+result = None
+
+print("Variable exploration: ")
+print(f"age         ={age}          (type: {type(age)})\n"
+      f"price       ={price}        (type: {type(price)})\n"
+      f"name        ={name}         (type: {type(name)})\n"
+      f"is_active   ={is_active}    (type: {type(is_active)})\n"
+      f"result      ={result}       (type: {type(result)})\n")
