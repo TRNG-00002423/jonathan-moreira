7c35ac4b8a1ddc9593d2a03f6f5e74032625b08f
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-02 11:49:33 -0400

    User Input

---
+++ Week1-Git-Python/exercises/Day1/qa-metrics-calculator/starter_code/metrics_calculator.py
@@ -0,0 +1,3 @@
+total_test_cases = int(input("What is the total number of test cases?: "))
+passed_tests = int(input("What is the tolta number of passed tests?: "))
+execution_time_in_seconds = float(input("What is the total execution time in seconds?: "))
