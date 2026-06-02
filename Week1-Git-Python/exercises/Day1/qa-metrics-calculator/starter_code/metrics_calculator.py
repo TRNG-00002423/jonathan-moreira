ece91a6e8f6de8799f8584ab9f7e37c1d5484e8c
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-02 11:57:09 -0400

    Calculate and display values

--- Week1-Git-Python/exercises/Day1/qa-metrics-calculator/starter_code/metrics_calculator.py
+++ Week1-Git-Python/exercises/Day1/qa-metrics-calculator/starter_code/metrics_calculator.py
@@ -7,5 +7,4 @@ failed_tests = total_test_cases - passed_tests
 pass_rate = passed_tests / total_test_cases
 fail_rate = failed_tests / total_test_cases
 average_time_per_test = total_execution_time_in_seconds / total_test_cases
-
 print(f"Total tests failed: {failed_tests}, Pass rate: {pass_rate * 100:.1f} %, Fail Rate: {fail_rate * 100:.1f}%, Avg time per test: {average_time_per_test:.2f}")
