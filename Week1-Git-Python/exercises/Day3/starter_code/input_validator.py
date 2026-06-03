533ea64666f9507d86a5f2403bd24094429ec951
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-03 13:45:15 -0400

    Validates at least one lowercase letter

--- Week1-Git-Python/exercises/Day3/starter_code/input_validator.py
+++ Week1-Git-Python/exercises/Day3/starter_code/input_validator.py
@@ -11,7 +11,11 @@ def validate_password(password):
         requirements["valid"] = False
         requirements["error"].append("Password must contain at least one uppercase letter")
 
+    if not any(char.islower() for char in password):
+        requirements["valid"] = False
+        requirements["error"].append("Password must contain at least one lowercase letter")
 
     return requirements
 
 print(validate_password("abc"))
+print(validate_password("ABCDEFGH"))    # no lower, no digit, no special
