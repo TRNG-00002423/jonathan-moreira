3457e9bd03cf65ede4ac716032e94c03a56b70e8
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-03 13:42:26 -0400

    Validates at least one uppercase letter

--- Week1-Git-Python/exercises/Day3/starter_code/input_validator.py
+++ Week1-Git-Python/exercises/Day3/starter_code/input_validator.py
@@ -4,8 +4,14 @@ def validate_password(password):
         "error" : []
     }
     if len(password) < 8:
+        requirements["valid"] = False
         requirements["error"].append("Password must be at least 8 characters long")
     
+    if not any(char.isupper() for char in password):
+        requirements["valid"] = False
+        requirements["error"].append("Password must contain at least one uppercase letter")
+
+
     return requirements
 
 print(validate_password("abc"))
