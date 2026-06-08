8b1b535636170e54e792e71c67c2ed7407e6b482
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-08 16:19:59 -0400

    Adding student

--- Week2-Python-Java/exercises/Day1/sms_mini_project/app.py
+++ Week2-Python-Java/exercises/Day1/sms_mini_project/app.py
@@ -25,6 +25,19 @@ def show_students():
 def show_student_by_id(id):
     return jsonify(students[id-1])
 
+@app.post("/students")
+def add_student():
+    student = request.get_json()
+
+    student_id = len(students) + 1
+    student["id"] = student_id
+    students.append(student)
+
+    return jsonify({"id" : student_id,
+                    "name" : student["name"],
+                    "course" : student["course"]})
+
+
 if __name__ == "__main__":
     # Runs in the developer test mode
     app.run(debug=True)
