294dcd16e38c3f06e339e28d79a4c683229cea4a
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-15 13:43:37 -0400

    Ascending sort implemented

--- Week3-Java-Advanced/exercises/Day1/starter_code/ArrayLoopsLab.java
+++ Week3-Java-Advanced/exercises/Day1/starter_code/ArrayLoopsLab.java
@@ -39,19 +39,50 @@ public class ArrayLoopsLab {
         return max;
         
     }
+
+    public static void sortAscending(int[] data){
+
+        for(int i = 0; i < data.length; i++)
+        {
+            for(int j = 0; j < data.length - i - 1; j++){
+                if(data[j] > data[j+1])
+                {
+                    int temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
+                }
+            }
+        }
+    } 
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,6};
+        int [] arr2 = {5,2,3,1,4,6};
+
+        System.out.println("Before Reverse");
         for(int num : arr)
         {
-            System.out.println(num);
+            System.out.print(num + " ");
         }
         reverse(arr);
+        System.out.println("\nAfter Reverse");
         for(int num : arr)
         {
-            System.out.println(num);
+            System.out.print(num + " ");
         }
 
-        System.out.println("Min: " + min(arr));
+        System.out.println("\nMin: " + min(arr));
         System.out.println("Max: " + max(arr));
+
+        System.out.println("Before Ascending Sort");
+        for(int num : arr2)
+        {
+            System.out.print(num + " ");
+        }
+        sortAscending(arr2);
+        System.out.println("\nAfter Ascending Sort");
+        for(int num : arr2)
+        {
+            System.out.print(num + " ");
+        }
     }
 }
