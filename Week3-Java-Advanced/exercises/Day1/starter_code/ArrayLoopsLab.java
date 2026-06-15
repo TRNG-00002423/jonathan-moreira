557d5e9ddc7a8f1206eeabadb18b4a1491747200
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-15 13:30:10 -0400

    Min & Max implemented

--- Week3-Java-Advanced/exercises/Day1/starter_code/ArrayLoopsLab.java
+++ Week3-Java-Advanced/exercises/Day1/starter_code/ArrayLoopsLab.java
@@ -9,6 +9,36 @@ public class ArrayLoopsLab {
         }
     }
 
+    public static int min(int [] data){
+        if(data == null || data.length == 0)
+            throw new IllegalArgumentException("Illegal Array Length");
+
+        int min = data[0];
+
+        for(int number : data){
+            if(number < min){
+                min = number;
+            }
+        }
+
+        return min;
+    }
+
+    public static int max(int [] data){
+        if(data == null || data.length == 0)
+            throw new IllegalArgumentException("Illegal Array Length");
+
+        int max = data[0];
+
+        for(int number : data){
+            if(number > max){
+                max = number;
+            }
+        }
+
+        return max;
+        
+    }
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5,6};
         for(int num : arr)
@@ -20,5 +50,8 @@ public class ArrayLoopsLab {
         {
             System.out.println(num);
         }
+
+        System.out.println("Min: " + min(arr));
+        System.out.println("Max: " + max(arr));
     }
 }
