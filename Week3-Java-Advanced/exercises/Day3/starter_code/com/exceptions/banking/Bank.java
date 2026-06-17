7a726886fc82861caf8e4eb3bac80badaa0eda9b
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-17 11:38:03 -0400

    Implemented bank class features

--- Week3-Java-Advanced/exercises/Day3/starter_code/com/exceptions/banking/Bank.java
+++ Week3-Java-Advanced/exercises/Day3/starter_code/com/exceptions/banking/Bank.java
@@ -1,16 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
-    // TODO HashMap<String, Account>
+    Map<String, Account> bank = new HashMap<>();
 
     public void openAccount(String id, double initialDeposit) throws InvalidAccountException {
-        throw new UnsupportedOperationException("TODO");
+        if(bank.containsKey(id) || id.isBlank())
+            throw new InvalidAccountException(id);
+
+        bank.put(id, new Account(id, initialDeposit));
+        System.out.println(getAccount(id));
     }
 
     public Account getAccount(String id) throws InvalidAccountException {
-        throw new UnsupportedOperationException("TODO");
+        if(bank.containsKey(id) || id.isBlank())
+            throw new InvalidAccountException(id);
+        
+        return bank.get(id);
     }
 
     public void transfer(String fromId, String toId, double amount)
             throws InvalidAccountException, InsufficientFundsException {
-        throw new UnsupportedOperationException("TODO");
+        if(bank.containsKey(fromId) || fromId.isBlank())
+            throw new InvalidAccountException(fromId);
+        if(bank.containsKey(toId) || toId.isBlank())
+            throw new InvalidAccountException(toId); 
+
+        Account accountFromId = getAccount(fromId);
+        accountFromId.withdraw(amount);
+        
+        Account accountToId = getAccount(toId);
+        accountToId.deposit(amount);
+
+        System.out.println(accountFromId);
+        System.out.println(accountToId);
+        
     }
 }
