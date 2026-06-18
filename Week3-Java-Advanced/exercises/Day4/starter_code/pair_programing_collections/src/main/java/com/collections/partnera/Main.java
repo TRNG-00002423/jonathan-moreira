6f9d8e970975fc170903f0ba45239f70b8bab489
Jonathan Moreira Alsina <yonathanm772@gmail.com>
2026-06-18 17:34:58 -0400

    Finished top frequent and unique words

--- Week3-Java-Advanced/exercises/Day4/starter_code/pair_programing_collections/src/main/java/com/collections/partnera/Main.java
+++ Week3-Java-Advanced/exercises/Day4/starter_code/pair_programing_collections/src/main/java/com/collections/partnera/Main.java
@@ -1,16 +1,21 @@
 package com.collections.partnera;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 import main.java.com.collections.partnera.WordCountComparator;
 
 public class Main {
     public static void main(String[] args) {
-        String paragraph = "Hello, hello! World.";
+        String paragraph = """
+            Java collections maps sets queues lambdas
+            Java maps and sets and more Java
+            """;
         String [] words = paragraph.toLowerCase().split("[^A-Za-z]+");
 
         for(String word : words)
@@ -24,18 +29,36 @@ public class Main {
 
         topFrequentWords(2, wordCount);
 
+        TreeSet<String> vocabulary = new TreeSet<>();
+
+        for (String word : words) {
+            if (!word.isBlank()) {
+                vocabulary.add(word);
+            }
+        }
+
+        System.out.println(vocabulary.first());
+        System.out.println(vocabulary.last());
+
     }
 
     public static void topFrequentWords(int n, Map<String,Integer> dic){
         List <Map.Entry<String, Integer>> words = new ArrayList<>();
 
         for (Map.Entry<String, Integer> en : dic.entrySet()) {
-            System.out.println(en);
-            words.add(en);
-            
+            words.add(en);      
         }
 
         Collections.sort(words, new WordCountComparator());
-        System.out.println(words);
+
+        int count = 0;
+        System.out.println("\nTop Frequent Words");
+        for(Map.Entry<String, Integer> w : words){
+            if(count == n)
+                break;
+
+            System.out.println(w);
+            count++;
+        }
     }
 }
